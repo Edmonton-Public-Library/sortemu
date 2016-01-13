693793cc68ba9fc0b925089ca052e0a3653c65cc
Andrew Nisbet <ANisbet@epl.ca>
2016-01-13 12:35:57 -0700

    Initialized project.

---
+++ Readme.md
@@ -0,0 +1,148 @@
+Project Notes
+-------------
+Initialized: Wed Nov 18 09:55:09 MST 2015.
+
+Sorter basic description:
+-----------------------
+An item received on an induction slot will be compared to the rules defined in a configuration file. The rules are read
+starting from the top, line-by-line until a rule fires and the item is shunted into the bin associated with the rule.
+The rule functions to define a sort route which just means bin number. If no rule matches the item travels through the
+sorter and ends up in the exception bin.
+
+Each sorter can have many bins. EPL has sorters ranging from 3 to 11 bins.
+
++---+---+---+---+---+---+
+|   | 10| 8 | 6 | 4 | 2 |
+| 11|---+---+---+---+---+   <- item travel direction
+|   | 9 | 7 | 5 | 3 | 1 |
++---+---+---+---+---+---+
+
+Organization of a 11 bin sorter. In this case bin 11 is the exception bin.
+
+Sorter configuration (the sort matrix):
+---------------------------------------
+Sort matrices on the 3M sorters is complicated, but logical. The sorters have a configuration file that is made up of rules, one per line.
+A complete list of configurable facets for a rule are:
+```
+  0 - Sort Route
+  1 - Alert
+  2 - AlertType
+  3 - Magnetic Media
+  4 - Media Type
+  5 - Permanent Location
+  6 - Destination Location
+  7 - Collection Code
+  8 - Call Number
+  9 - Sort Bin	Branch ID
+ 10 - Library ID
+ 11 - Check-in Result
+ 12 - Custom Tag Data
+ 13 - Detection Source
+```
+We only use Alert, Alert Type, Permanent Location (Home location), Destination Location (Item Library), Collection code (iType),
+and call number. Below is a typical entry.
+```
+Sort Route	Alert	AlertType	Magnetic Media	Media Type	Permanent Location	Destination Location	Collection Code	Call Number	Sort Bin	Branch ID	Library ID	Check-in Result	Custom Tag Data	Detection Source
+R5	*	*	*	*	NONFICTION, REFERENCE	*	BOOK, JBOOK, PBK, PAPERBACK	7*,8*,9*	*	*	*	*
+```
+
+The above mentioned rules are defined within a web interface on each induction unit.
+The configuration can be exported, then imported to another sorter induction unit. The exported configuration file is
+called '3M SelfCheck System Configuration.cfg' by default and is binary encoded.
+
+Sorters may have many induction slots; places where materials are fed into the machine, but all induction slots fall into 2 categories:
+Staff side or Patron side. Each induction unit needs to be configured independently, and it is generally better to work on the configuration
+for the staff side because it is much easier to test and see what happens. Once testing is complete, the configuration can be exported, then
+uploaded to the patron side(s). This will eliminate confusion where patron side sorting differs from staff side.
+In addition to the staff and patron induction units there is a central control unit that starts and maintains sorter machinery operations.
+
+The emulator:
+-------------
+
+In this emulator we will be concerned with the bin, Permanent location, Destination Location, Collection Code, and Call number.
+The bin is the final destination of the items, the other 4 values will be supplied in pipe delimited form or specified from a file.
+The configuration file, for the emulator, will just consist of the 4 key criteria specified as they are shown in the web interface.
+That is 
+ - '*' means match anything in this criterial, or everything matches.
+ - Permanent location must contain full or partial home locations such as NONFICTION, or in the case of a partial specification PBK*, which
+will match any home location that starts with 'PBK'
+ - Similarly, collection code must match fully qualified item types like JBOOK or partial item types such as BLU-RAY* which will match 
+BLU-RAY21, and BLU-RAY14.
+ - Call numbers are similar and work as follows 700,800,900 specifies exactly 7, 8, and 900 call numbers. To match ranges use 02* for any
+range in '02X' range, and 9* to match any call number in the 900 range. 
+
+Once the config file is read in it is parsed for errors, then sorteremu starts reading in the items also one per line, pipe separated on
+STDIN. There must always be 4 pipes per line but call number or destination location may be empty if there are no rules to match. Once a 
+rule is entered that specifies a condition the line must include values or it will sort to the exception bin, read fail on all lines and 
+end up in the default bin. 
+
+Instructions for creating a configuration file:
+-----------------------------------------------
+There are 2 ways to create configuration files for input. The first is to select all the lines from the sorter's web interface
+copy, and paste in a new text document. The extension of the file does not matter. Once created the emulator will parse
+the rules from the file and test them for redundant rules and under utilized bins. An example is shown below.
+```
+R9	Y	03	*	*	*	*	*	*	*	*	*	*	*	*
+Submit	 	Submit	Submit	R9	Y	02	*	*	*	*	*	*	*	*	*	*	*	*
+Submit	 	Submit	Submit	R9	Y	01	*	*	*	*	*	*	*	*	*	*	*	*
+Submit	 	Submit	Submit	R11	*	*	*	*	*	*	BESTSELLER, FLICKSTOGO, JBESTSELLR, JFLICKTOGO, JFLICKTUNE, TUNESTOGO	*	*	*	*	*	*	*
+Submit	 	Submit	Submit	R7	*	*	*	*	NONFICTION, REFERENCE	*	BOOK, JBOOK, PBK, PAPERBACK	00*,01*,02*,03*,04*,05*,06*,07*,08*,09,1*,2*,3*,4*,5*,6*	*	*	*	*	*	*
+...
+```
+These rules are taken directly from a 3M sorter matrix web page (version 3.50.050.0).
+
+The second way is to craft a rule file by hand. here is an example of 2 basic rule definitions created by hand.
+```
+R4|*|*|*|*|*|PBK*|*|*|
+R5|*|*|*|*|*|BOOK|900|*
+```
+
+TODO: A third method is to use screen scraping via URL.
+
+
+Instructions for creating input item data:
+------------------------------------------
+
+The emulator will test items to determine which route rules (bin) will fire. You can create an input file for the -i
+switch with
+```
+echo 31221115689585 | selitem -iB -oNBlyt | selcallnum -iN -oSA
+31221114889871  |YRCA|EPLMNA|JPBK|CHILDREN'S FICTION - SERIES S PBK|
+```
+The emulator takes these fields and pads missing columns so they are the same number of columns as the rule, then performs
+a comparison, reporting which rule fires and why.
+
+Once the rules and input data are aligned the emulator will test each item and make a report of the success of each item.
+Here is an example. For the input of:
+```
+31221062470567  |ABORIGINAL|EPLMNA|JBOOK|E SMI
+31221114889871  |YRCA|EPLMNA|JPBK|CHILDREN'S FICTION - SERIES S PBK
+```
+The output generated is as follows.
+```
+item: 31221062470567  ->bin 7, config line 2, SR:R7
+item: 31221114889871  ->bin 4, config line 10, SR:R4
+```
+
+Instructions for Running:
+-------------------------
+python sortemu.py [-i'items.lst'] -c'matrix.cfg' [-e]
+
+Product Description:
+--------------------
+Perl script written by Andrew Nisbet for Edmonton Public Library, distributable by the enclosed license.
+
+Repository Information:
+-----------------------
+This product is under version control using Git.
+[Visit GitHub](https://github.com/Edmonton-Public-Library)
+
+Dependencies:
+-------------
+None
+
+Known Issues:
+-------------
+The emulator does not currently take the current hold state of test items into account during processing, that is,
+what would happen to these items if holds were not taken into account?
+TODO: Allow for items with holds.
---
+++ sortemu.py
@@ -0,0 +1,379 @@
+#!/usr/bin/env python
+####################################################
+#
+# Python source for project sorteremu_py
+# Purpose:
+# Method:
+#
+# <one line to give the program's name and a brief idea of what it does.>
+#    Copyright (C) 2015  Andrew Nisbet
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# Author:  Andrew Nisbet, Edmonton Public Library
+# Created: Fri Dec 18 10:23:18 MST 2015
+# Rev:
+#          0.0 - Dev.
+#
+####################################################
+import sys
+import getopt
+import os
+import re
+from itertools import product # Produces product of vector of rules for analysis
+
+
+# A rule is a object that encapsulates a single AND operation, so represents data from a single column within
+# a configuration file. If a rule is provided as a string it is assumed to be either a single rule or a rule set
+# Where different values are permitted if separated by a ',' and optional space. In either case the rule is stored
+# and query-able.
+# Accepts either a string with optional ',', or a list.
+class Rule:
+    def __init__(self, rules):
+        # parse the rules depending on the type of input. String or list.
+        self.rule = []
+        assert isinstance(rules, list)
+        for rule in rules:
+            print rule
+
+
+# Rule engine reads and stores rules, then tests can be run against arbitrary items.
+# Input must have the following structure: 'item_id|home_location|destination_library|item_type|call_number (range)|'
+# produced with echo 31221115689585 | selitem -iB -oNBlyt | selcallnum -iN -oSA
+# Produces: '31221115689585  |PBKMYS|EPLSTR|BOOK|870.44|'
+#  0 - Sort Route
+#  1 - Alert
+#  2 - AlertType
+#  3 - Magnetic Media
+#  4 - Media Type
+#  5 - Permanent Location
+#  6 - Destination Location
+#  7 - Collection Code
+#  8 - Call Number
+#  9 - Sort Bin	Branch ID
+# 10 - Library ID
+# 11 - Check-in Result
+# 12 - Custom Tag Data
+# 13 - Detection Source
+# R5	*	*	*	*	NONFICTION, REFERENCE	*	BOOK, JBOOK, PBK, PAPERBACK	7*,8*,9*	*	*	*	*
+# This script is based on the common rules I have seen on our sorters. That is we don't use position 1,2,3, or 4
+# but commonly use 5, 7, and 8. If you add additional rules in 1,2,3,4 or after 8, you can and they will be checked
+# but the script will fail if you don't add positional matching data in your input.
+#
+# Rule engine can read configs specified from text files that have raw screen scraping from the configuration web page.as
+# TODO: add a switch to specify a URL so the app can scrape the config itself.
+class RuleEngine:
+    def __init__(self):
+        self.MIN_COLS = 9
+        self.rule_table = []
+        self.rule_column_names = [ 'SortRoute', 'Alert', 'AlertType', 'MagneticMedia', 'MediaType',
+                              'PermanentLocation', 'DestinationLocation', 'CollectionCode',
+                              'CallNumber', 'SortBin', 'BranchID', 'LibraryID', 'CheckinResult',
+                              'CustomTagData', 'DetectionSource' ]
+
+    # Tests for duplicate rule entries.
+    def test_duplicates(self, explain=False):
+        # Create a hash map into which we put keys made of the strings
+        # of all combinations of rules on a given line,  and-ed together.
+        # Once done, if the key appears twice it is has an identical twin.
+        count = 0
+        master_rule_map = {}
+        # for item_entry in self.rule_table:
+        for line in self.rule_table:
+            my_item_list = []
+            rule_name = line.pop(0)
+            for item_entry in line:
+                if item_entry[0] == '*':
+                    continue
+                elif item_entry.find(','):
+                    my_item_list.append(item_entry.split(','))
+                else:
+                    my_item_list.append(item_entry)
+            # Now create a string for each rule that is a concatenation of all the and'ed rules for this line.
+            for item_entry in list(product(*my_item_list)):
+                rule_string = ''
+                for item in item_entry:
+                    rule_string += item + '.'
+                count += 1
+                # Chop off the trailing '.' for cleaner display
+                rule_string = rule_string[:-1]
+                if explain:
+                    sys.stdout.write("{0}) {1}::".format(count, rule_name))
+                    sys.stdout.write("{0}\n".format(rule_string))
+                # Add each new name as a key into a hashmap with the rule name as the value.
+                if master_rule_map.has_key(rule_string):
+                    sys.stdout.write("** Warning duplicate rule detected in rule {0} and {1}->{2}\n".format(master_rule_map[rule_string], rule_name, rule_string.split('.')))
+                else:
+                    master_rule_map[rule_string] = rule_name
+            # Put the route name back on the list for reference later during item check.
+            line.insert(0, rule_name)
+
+    # Loads a rule from the config file, adjusting to 2 different types of formatting. The config file can be created
+    # by hand separating the columns with pipes, but that got boring so the method can also parse values that are cut
+    # and paste from the sorter's configuration web page. No extra editing required. See parse_screen_scrape_config().
+    def load_rule(self, rule_line):
+        this_line_list = rule_line.split('|')
+        # No pipes means the input is probably screen-scraped from the configuration web page.
+        if len(this_line_list) == 1:
+            this_line_list = self.parse_screen_scrape_config(rule_line)
+        this_line_list[-1] = str.strip(this_line_list[-1])
+        if this_line_list[-1] == '':
+            this_line_list[-1] = '*'
+        for i in range(len(this_line_list), len(self.rule_column_names)): # pad remaining fields with stars
+            this_line_list.insert(i, '*')
+        # sys.stdout.write("padded rule::{0}\n".format(this_line_list))
+        if len(this_line_list) >= self.MIN_COLS:
+            new_list = []
+            for col in this_line_list:
+                if col != '\n': # Sometimes Symphony users will include a trailing '|' which will cause and empty field.
+                    new_list.append(col)
+            self.rule_table.append(new_list)
+            # sys.stdout.write("****{0}\n".format(new_list))
+        else:
+            sys.stderr.write('** "{0}" not enough rules, ignoring.'.format(rule_line))
+            sys.stderr.write('** expected {0}, but got {1}.'.format(self.MIN_COLS, len(this_line)))
+
+    # Sometimes you want to set up a config file based on what the settings are on the target machine.
+    # If you go into the sorter configuration you can copy and paste the contents of the config HTML page
+    # into a text file. This script will clean it up and add the rules.
+    # REJECT	Y	01	*	*	*	*	*	*	*	*	*	*	*	*
+    # Submit	 	Submit	Submit	REJECT	Y	02	*	*	*	*	*	*	*	*	*	*	*	*
+    # Submit	 	Submit	Submit	REJECT	Y	03	*	*	*	*	*	*	*	*	*	*	*	*
+    # Submit	 	Submit	Submit	R2	*	*	*	*	*	*	CD, DVD*, JCD, VIDGAME, BLU-RAY*	*	*	*	*	*	*	*
+    # Submit	 	Submit	Submit	R5	*	*	*	*	*	EPLCLV	PERIODICAL	*	*	*	*	*	*	*
+    # Submit	 	Submit	Submit	R6	*	*	*	*	*	EPLCLV	JPERIODICL	*	*	*	*	*	*	*
+    # Submit	 	Submit	Submit	R1	*	*	*	*	TEENFIC, TEENGRAPHC, TPBK, TPBKSER, EASYENGL	*	JBOOK, JPBK, BOOK	*	*	*	*	*	*	*
+    # Submit	 	Submit	Submit	R4	*	*	*	*	ABORIGINAL, JUVPIC, JPBK, JUVNONF, JUVOTHLANG, NONFICTION, YRCA	*	JPBK, JPAPERBACK, JBOOK, JOTHLANGBK	*	*	*	*	*	*	*
+    # Submit	 	Submit	Submit	R6	*	*	*	*	EMOVIE, JUV*, COMICBOOK	*	JBOOK, JDVD*, JBLU-RAY*, JPBK, COMIC	*	*	*	*	*	*	*
+    # Submit	 	Submit	Submit	R1	*	*	*	*	*	*	OTHLANGBK	*	*	*	*	*	*	*
+    # Submit	 	Submit	Submit	R3	*	*	*	*	GENERAL, FIC*, PBK*, LARGE*	*	BOOK, LARGEPRINT, PBK, PAPERBACK	*	*	*	*	*	*	*
+    # Submit	 	Submit	Submit	R5	*	*	*	*	*	*	BOOK, MUSICSCORE, PAPERBACK	*	*	*	*	*	*	*
+    def parse_screen_scrape_config(self, line_of_rules):
+        new_line = str.replace(line_of_rules, 'Submit', '')
+        new_line = str.strip(new_line)
+        new_line = str.replace(new_line, ', ', ',')
+        new_line = re.sub('\s+', '|', new_line)
+        new_line_list = new_line.split('|')
+        # now remove the space after commas
+        # print str(new_line_list)
+        return new_line_list
+
+    # Displays all the rules in order.
+    def show_rules(self):
+        for rule in self.rule_table:
+            sys.stdout.write("{0}".format(rule))
+
+    # Checks the rules for errors and possible optimizations.
+    # Each rule has a number of qualifiers that must match for the rule to fire. If a rule has fewer qualifiers
+    # but appears first, it will fire before a rule with more details, negating the need for the additional complexity.
+    # If we arrange each rule in a tree so that every combination of rule is computed we can find mis-ordered rules
+    # (redundant) and conflicting rules (repeated).
+    # Redundant example:
+    # BOOK|*|*|
+    # BOOK|*|9*| - never fires because simpler rule takes precedence.
+    # Conflicting example:
+    # CD,DVD*|*|*|
+    # DVD21,JCD,JDVD|*|*| - DVD21 never fires because DVD* matches first.
+    # If we put these rules into lists like so:
+    # [BOOK]
+    # [BOOK, 9]
+    # we can see that the simpler rule will fire first since there are more matches that must be met within the.
+    # In the second case we search similar rules by cherry picking the '*' candidates and determine if there are rules
+    # that follow the current rule that are more refined.
+    # grep DVD* in [DVD21,JCD,JDVD]
+    def test_rules(self, explain=False):
+        # now we want to expand all the lists within a line so that each member creates a new list.
+        sys.stdout.write('testing bins.\n')
+        self.check_bins(explain)
+        sys.stdout.write('done.\n')
+        sys.stdout.write('testing for redundant rules.\n')
+        self.test_duplicates(explain)
+        sys.stdout.write('done.\n')
+
+    # Checks if all the bins are utilized.
+    def check_bins(self, explain=False):
+        bins = []
+        for i in range(0, len(self.rule_table)):
+            # Snag the bin number and put in an array then sort.
+            my_bin_name = self.rule_table[i][0]
+            if my_bin_name == 'REJECT' or my_bin_name == 'reject':
+                sys.stdout.write('* WARNING: sort route #{0} is set up to reject materials.\n'.format(i + 1))
+                continue
+            bins.append(my_bin_name)
+        # now produce a histogram of rules for each bin.
+        bin_dict = {}
+        for bin in bins:
+            try:
+                bin_dict[bin] += 1
+            except KeyError:
+                bin_dict[bin] = 1
+        sys.stdout.write('found {0} bins with routing rules.\n'.format(len(bin_dict.keys())))
+        bin_key_list = bin_dict.keys()
+        bin_key_list.sort()
+        for name in bin_key_list:
+            sys.stdout.write('bin #{0} has {1} rules.\n'.format(name, bin_dict[name]))
+
+    # Tests an items data from the ILS and returns a list of the [ item_id, is_match, rule# ]
+    # param:  single rule line from the matrix.
+    # param:  item data read from the item file.
+    # pamam:  boolean value True will show where the rule fails, False returns quietly.
+    # return: list of the [ 31221012345678, True, R6 ]
+    def is_rule_match(self, rule, item_line, show_fail=True):
+        line_items = item_line.split('|')
+        # sys.stdout.write('>>>>item cols:{0}\n>>>>rule cols:{1}.\n'.format(line_items, rule))
+        if len(line_items) != len(rule):
+            sys.stdout.write('columns don\'t match item cols:{0}, rule cols:{1}.\n'.format(len(line_items), len(rule)))
+            sys.exit(1)
+        # Since we do a side by side comparison of config columns to item columns
+        # we need to ensure that the item_line has the same number of columns.
+        # The data arrives from
+        # columns of input must match rule columns, and each value
+        # within the data column must match at least one rule specified
+        # in the rule columns for the test to succeed.
+        # the bin number and in the input its the item id.
+        match_count = 1 # the first col of a rule and of the item don't have to match.
+        matched_rules = []
+        for index in range(1, len(line_items)):
+            regexes = rule[index].split(',') # regexes can look like BOOK,PBK*
+            test_col = line_items[index]
+            no_match_count = 0
+            for reg in regexes:
+                regex = str.strip(reg)
+                regex = str.replace(regex,'*','')
+                if show_fail:
+                    sys.stdout.write('"{0}" <=> "{1}", '.format(reg, test_col))
+                # If the regex is empty, it matches anything.
+                # It can be empty because we remove the '*' because of python 'bug'.
+                if len(regex) == 0: # That was a star so everything automatically matches.
+                    if show_fail:
+                        sys.stdout.write(' Auto-MATCH.\n')
+                    break
+                elif test_col == '*': # and len(regex) > 0 because previous if failed.
+                    if show_fail:
+                        sys.stdout.write(' NO-MATCH.\n')
+                    break # It doesn't matter what the rule wants we don't have enough information to test.
+                elif test_col.startswith(regex):
+                    if show_fail:
+                        sys.stdout.write(' MATCHES\n'.format(test_col, regex))
+                    matched_rules.append(regex)
+                    break
+                no_match_count += 1
+                # if we got here none of the listed rules in this column matched our items data.
+                if no_match_count == len(regexes):
+                    return [line_items[0], False, rule[0], matched_rules]
+                if show_fail:
+                    sys.stdout.write(" skip...\n")
+            index += 1
+        # line_items.insert(0, item_id)
+        return [line_items[0], len(matched_rules) > 0, rule[0], matched_rules]
+
+    # Tests an item against the matrix, reports which bin it gets sent to and why.
+    # Input must have the following structure: 'item_id|home_location|destination_library|item_type|call_number (range)|'
+    # produced with echo 31221115689585 | selitem -iB -oNBlyt | selcallnum -iN -oSA
+    # 31221115689585  |PBKMYS|EPLLHL|PBK|MYSTERY B TRADEPBK|
+    # Which gets expanded to include extra '*' characters standing in for config settings we don't normally use.
+    # '31221115689585  |PBKMYS|EPLSTR|BOOK|870.44|'
+    def test_item(self, item, explain=True):
+        # the rules must match top down
+        my_item = str.strip(item)
+        if my_item.endswith('|'):
+            my_item = my_item[:-1]
+        item_columns = my_item.split('|')
+        # TODO: add processing to allow the first three rules to fire if the item has holds on the ILS. In the mean
+        # time the script just works on the general case of assuming there are no holds for these items, to see where
+        # they would theoretically fall into.
+        # sys.stdout.write('item_array:{0}\n'.format(item_columns))
+        for i in range(1, len(self.rule_column_names)): # instead of sortroute we have an item id.
+            if i < 5: # put stars in the following positions to match rule columns.
+                item_columns.insert(i, '*')
+            if i > 8:
+                item_columns.append('*')
+        # Convert back to string
+        item_string = '|'.join(item_columns)
+        # Test print item with complete columns.
+        if explain:
+            sys.stdout.write('item_string:{0}\n'.format(item_string))
+        rule_index = 1
+        for rule in self.rule_table:
+            if rule[1] == 'Y' or rule[1] == 'N':
+                rule_index += 1
+                continue
+            result = self.is_rule_match(rule, item_string, explain)
+            # result = self.is_rule_match(rule, item_string)
+            if result[1]:
+                sys.stdout.write("line {1}: {0}->bin {3}({2}) matches on {4}".format(item_columns[0], rule_index, result[2], result[2][1:], result[3]))
+                if explain:
+                    sys.stdout.write(", matched on rule '{0}'.\n".format(result[3]))
+                else:
+                    sys.stdout.write("\n")
+                break
+            rule_index += 1
+
+
+def usage():
+    sys.stderr.write('Usage:\n')
+    sys.stderr.write('  notice.py -i <input_file> -c <config_file>\n')
+    sys.stderr.write('  Processes Symphony reports into printable notice format\n')
+
+
+# Take valid command line arguments -b'n', -o, -i, -d, and -h -s.
+def main(argv):
+    config_file = ''
+    items_file = ''
+    explain = False
+    try:
+        opts, args = getopt.getopt(argv, "c:ei:", ["config=", "items="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit()
+    for opt, arg in opts:
+        if opt in ("-c", "--config"):
+            assert isinstance(arg, str)
+            config_file = arg
+        elif opt in ("-i", "--items"):
+            assert isinstance(arg, str)
+            items_file = arg
+        elif opt in "-e":
+            explain = True
+    print 'configuration file is = ', config_file
+    sys.stderr.write('running file ' + items_file + '\n')
+    if not os.path.isfile(config_file):
+        sys.stderr.write("** error: configuration file {0} does not exist.\n".format(config_file))
+        sys.exit()
+    if items_file and not os.path.isfile(items_file):
+        sys.stderr.write("** error: item(s) file {0} does not exist.\n".format(items_file))
+        sys.exit()
+    rule_engine = RuleEngine()
+    c_file = open(config_file, 'r')
+    for line in c_file:
+        rule_engine.load_rule(line)
+    c_file.close()
+    # Now you can check rules if you like
+    # rule_engine.check_rules(explain)
+    rule_engine.test_rules(explain)
+    if items_file:
+        i_file = open(items_file, 'r')
+        for item in i_file:
+            rule_engine.test_item(item, explain)
+        i_file.close()
+
+    sys.exit(0)
+
+if __name__ == "__main__":
+    # import doctest
+    # doctest.testmod()
+    main(sys.argv[1:])
+    # EOF
